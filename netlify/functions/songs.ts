8e25c95959818e9700ba8dd40e53bca890d13103
corey <csmaller@elumatherapy.com>
2025-10-13 12:26:17 -0600

    order by

--- netlify/functions/songs.ts
+++ netlify/functions/songs.ts
@@ -16,11 +16,15 @@ export const handler: Handler = async (event) => {
   try {
     const q = event.queryStringParameters?.q;
     
-    const where =[
+    const where = q
+      ? {
+          OR: [
             { title: { contains: q, mode: 'insensitive' } },
             { artist: { contains: q, mode: 'insensitive' } },
-            {orderBy: {title: 'asc'}}]
-         
+            {orderBy: {title: 'asc'}},
+          ],
+        }
+      : undefined;
     const songs = await (prisma as any).songs.findMany({ where });
     return { statusCode: 200, headers, body: JSON.stringify(songs) };
   } catch (err) {
